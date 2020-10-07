81c29eddc8e08d542d140ba04ee2293902f54c62
Drew A <alvarez.drew@gmail.com>
2020-10-06 19:39:54 -0500

    fixed some formating

--- server.js
+++ server.js
@@ -15,35 +15,38 @@ var notes = require("./db/db.json");
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
   });
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-  app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-  });
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", function(req, res) {
+  return res.json(notes);
+});
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-  app.post("/api/notes", function(req, res) {
-    var note = req.body;
-    if(notes.length === 0){
-      Object.assign(note, {"id": "0"})
-    }else{
-      var lastId = parseInt(notes[notes.length -1].id);
-      Object.assign(note, {"id": (lastId + 1).toString()})
-    }
-    console.log(note);
-    notes.push(note);
-    res.json(note);
-  });
+app.post("/api/notes", function(req, res) {
+  var note = req.body;
+  if(notes.length === 0){
+    Object.assign(note, {"id": "0"})
+  }else{
+    var lastId = parseInt(notes[notes.length -1].id);
+    Object.assign(note, {"id": (lastId + 1).toString()})
+  }
+  console.log(note);
+  notes.push(note);
+  res.json(note);
+});
 
-  app.delete("/api/notes/:id", function(req, res){
-    var chosen = req.params.id;
-    for (var i = 0; i < notes.length; i++){
-      if (chosen === notes[i].id)
-      notes.splice([i],1)
-    }
-    return res.json(notes)
-  })
+app.delete("/api/notes/:id", function(req, res){
+  var chosen = req.params.id;
+  for (var i = 0; i < notes.length; i++){
+    if (chosen === notes[i].id)
+    notes.splice([i],1)
+  }
+  return res.json(notes)
+})
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
